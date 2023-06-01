9be9b66d903824289c43d9715b8592ecb370a7dc
Devin Matte <devinmatte@gmail.com>
2023-05-31 20:11:43 -0400

    Adding Linting CI (#16)
    * Adding linting CI to follow suit of other apps
    
    * Fixing CI file
    
    * Running black formatting on all python
    
    * Update .github/workflows/lint.yml
    
    Co-authored-by: Nathan Weinberg <31703736+nathan-weinberg@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Nathan Weinberg <31703736+nathan-weinberg@users.noreply.github.com>

--- ingestor/app.py
+++ ingestor/app.py
@@ -6,24 +6,27 @@ from chalicelib import (
     bluebikes,
 )
 
-app = Chalice(app_name='ingestor')
+app = Chalice(app_name="ingestor")
+
 
 ################
 # STORE ALERTS
 # Every day at 10am UTC: store alerts from the past
 # It's called yesterday for now but it's really two days ago!!
-@app.schedule(Cron(0, 10, '*', '*', '?', '*'))
+@app.schedule(Cron(0, 10, "*", "*", "?", "*"))
 def store_yesterday_alerts(event):
     two_days_ago = date.today() - timedelta(days=2)
     s3_alerts.store_alerts(two_days_ago)
 
+
 #################
 # STORE NEW TRAIN TRIPS
 # Ever day at 10:05am UTC: store new train runs from the previous day
-@app.schedule(Cron(5, 10, '*', '*', '?', '*'))
+@app.schedule(Cron(5, 10, "*", "*", "?", "*"))
 def store_new_train_runs(event):
     yesterday = date.today() - timedelta(days=1)
     new_trains.update_all(yesterday)
+
 
 #################
 # PROCESS & STORE SLOWZONES
@@ -32,17 +35,19 @@ def store_new_train_runs(event):
 
 #################
 # STORE BLUEBIKES FEED
-@app.schedule(Cron('0/5', '*', '*', '*', '?', '*'))
+@app.schedule(Cron("0/5", "*", "*", "*", "?", "*"))
 def bb_store_station_status(event):
     bluebikes.store_station_status()
 
+
 # 10am UTC -> 6am EST
-@app.schedule(Cron(0, 10, '*', '*', '?', '*'))
+@app.schedule(Cron(0, 10, "*", "*", "?", "*"))
 def bb_store_station_info(event):
     bluebikes.store_station_info()
 
+
 # 6am UTC -> 2am EST
-@app.schedule(Cron(0, 6, '*', '*', '?', '*'))
+@app.schedule(Cron(0, 6, "*", "*", "?", "*"))
 def bb_calc_daily_stats(event):
     yesterday = date.today() - timedelta(days=1)
     bluebikes.calc_daily_stats(yesterday)
