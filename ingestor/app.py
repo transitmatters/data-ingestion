0b51dc6c8b4c75ee2f4e007ffecf7788901960bf
Patrick Cleary <patcleary11@gmail.com>
2023-07-09 22:10:00 -0400

    Fix commit history

--- ingestor/app.py
+++ ingestor/app.py
@@ -61,7 +61,7 @@ def bb_calc_daily_stats(event):
 
 # Runs every 5 minutes from either 4 AM -> 1:55AM or 5 AM -> 2:55 AM depending on DST
 @app.schedule(Cron("0/5", "0-6,9-23", "*", "*", "?", "*"))
-def update_daily_speed_table(event):
+def update_delivered_trip_metrics(event):
     today = datetime.now()
     """ Update yesterdays entry until 4/5 am (9 AM UTC)"""
     if today.hour < 9:
@@ -72,7 +72,7 @@ def update_daily_speed_table(event):
 # 7am UTC -> 2/3am ET
 # Update weekly and monthly tables. At 2/3 AM EST and also after we have updated yesterday's data.
 @app.schedule(Cron(10, "7,12", "*", "*", "?", "*"))
-def update_weekly_and_monthly_tables(event):
+def update_agg_trip_metrics(event):
     agg_speed_tables.update_tables("weekly")
     agg_speed_tables.update_tables("monthly")
 
@@ -80,7 +80,7 @@ def update_weekly_and_monthly_tables(event):
 # 12 UTC -> 7/8am ET
 # The MBTA cleans up their data the next day (we suspect sometime after 4 AM). Update yesterday's data after this (and 2 days ago to be safe).
 @app.schedule(Cron(0, 12, "*", "*", "?", "*"))
-def update_yesterday_speeds(event):
+def update_delivered_trip_metrics_yesterday(event):
     today = datetime.now()
     daily_speeds.update_daily_table(today - timedelta(days=1))
     daily_speeds.update_daily_table(today - timedelta(days=2))
@@ -105,20 +105,19 @@ def update_speed_restrictions(event):
     speed_restrictions.update_speed_restrictions()
 
 
-# Manually triggered lambda for populating monthly or weekly tables. Only needs to be ran once.
+# Manually triggered lambda for populating daily trip metric tables. Only needs to be ran once.
 @app.lambda_function()
-def populate_weekly_or_monthly_tables(params, context):
-    """
-    line options: line-red | line-orange | line-blue
-    range options: weekly | monthly
-    """
-    agg_speed_tables.populate_table(params["line"], params["range"])
-
-
-# Manually triggered lambda for populating daily tables. Only needs to be ran once.
-# Takes line key as input (line-red | line-orange | line-blue)
-@app.lambda_function()
-def populate_daily(params, context):
+def populate_delivered_trip_metrics(params, context):
     start_date = datetime.strptime("2016-01-15", constants.DATE_FORMAT_BACKEND)
     end_date = datetime.now()
-    daily_speeds.populate_daily_table(start_date, end_date, params["line"])
+    for route in constants.ALL_ROUTES:
+        daily_speeds.populate_daily_table(start_date, end_date, route[0], route[1])
+
+
+# Manually triggered lambda for populating monthly or weekly tables. Only needs to be ran once.
+@app.lambda_function()
+def populate_agg_delivered_trip_metrics(params, context):
+    for line in constants.LINES:
+        print(line)
+        agg_speed_tables.populate_table(line, "monthly")
+        agg_speed_tables.populate_table(line, "weekly")
---
+++ ingestor/chalicelib/stations.py
@@ -0,0 +1,74 @@
+STATIONS = {
+    # RED LINE
+    "DAVIS": {
+        "SB": 70063,
+        "NB": 70064,
+    },
+    "SHAWMUT": {
+        "SB": 70091,
+        "NB": 70092,
+    },
+    "QUINCY_ADAMS": {
+        "SB": 70103,
+        "NB": 70104,
+    },
+    # ORANGE LINE
+    "MALDEN_CENTER": {
+        "SB": 70034,
+        "NB": 70035,
+    },
+    "GREEN_STREET": {
+        "SB": 70002,
+        "NB": 70003,
+    },
+
+    # BLUE LINE
+    "REVERE_BEACH": {
+        "SB": 70057,
+        "NB": 70058,
+    },
+    "GOV_CENTER_BLUE": {
+        "SB": 70039,
+        "NB": 70040,
+    },
+
+    # GREEN LINE
+    "SOUTH_ST": {
+        "SB": 70111,
+        "NB": 70110,
+    },
+    "BOYLSTON": {
+        "SB": 70159,
+        "NB": 70158,
+    },
+    "ENGLEWOOD": {
+        "SB": 70235,
+        "NB": 70236,
+    },
+    "GOV_CENTER_GREEN": {
+        "SB": 70202,
+        "NB": 70201,
+    },
+    "WOODLAND": {
+        "SB": 70163,
+        "NB": 70162,
+    },
+    "LECHMERE": {
+        "SB": 70502,
+        "NB": 70501,
+    },
+    "BACK_OF_THE_HILL": {
+        "SB": 70257,
+        "NB": 70258,
+    },
+    "BALL_SQ": {
+        "SB": 70510,
+        "NB": 70509,
+    },
+    "PARK_ST": {
+        "SB": [70196, 70197, 70198, 70199],
+        "NB": 70200,
+    },
+
+
+}
