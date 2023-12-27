40f1d7e1e15dc3ffe549da87fa9a924413eb455e
Devin Matte <devinmatte@gmail.com>
2023-12-26 20:52:48 -0500

    Adding commuter rail ridership data

--- ingestor/chalicelib/ridership/config.py
+++ ingestor/chalicelib/ridership/config.py
@@ -5,3 +5,5 @@ RIDERSHIP_BOX_URL = "https://massdot.app.box.com/s/21j0q5di9ewzl0abt6kdh5x8j8ok9
 RIDERSHIP_BUS_XLSX_REGEX = re.compile(r"Weekly_Bus_Ridership_by_Route_(\d{4})\.(\d{1,2})\.(\d{1,2})", re.I)
 
 RIDERSHIP_SUBWAY_CSV_REGEX = re.compile(r"(\d{4})\.(\d{1,2})\.(\d{1,2}) MBTA Gated Station Validations by line", re.I)
+
+CR_RIDERSHIP_ARCGIS_URL = "https://opendata.arcgis.com/api/v3/datasets/e2635c945f5b47a7923e0ee441b040c8_0/downloads/data?format=csv&spatialRefId=4326&where=1=1"
--- ingestor/chalicelib/ridership/dynamo.py
+++ ingestor/chalicelib/ridership/dynamo.py
@@ -2,10 +2,12 @@ import boto3
 from typing import Dict, List
 from datetime import datetime
 
+DYNAMO_TABLE_NAME = "Ridership"
+
 
 def ingest_ridership_to_dynamo(entries_by_line_id: Dict[str, List[Dict]]):
     dynamodb = boto3.resource("dynamodb")
-    Ridership = dynamodb.Table("Ridership")
+    Ridership = dynamodb.Table(DYNAMO_TABLE_NAME)
     with Ridership.batch_writer() as batch:
         for line_id, entries in entries_by_line_id.items():
             for entry in entries:
@@ -13,7 +15,7 @@ def ingest_ridership_to_dynamo(entries_by_line_id: Dict[str, List[Dict]]):
                 batch.put_item(
                     Item={
                         "lineId": line_id,
-                        "count": entry["count"],
+                        "count": int(entry["count"]),
                         "date": entry["date"],
                         "timestamp": int(dt.timestamp()),
                     }
