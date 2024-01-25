78f3e9982009f85dc157be000d3c3882a42082d4
JD Rudie <rudiejd@miamioh.edu>
2024-01-25 09:42:27 -0500

    feat(shuttles): still messing about

--- ingestor/chalicelib/yankee.py
+++ ingestor/chalicelib/yankee.py
@@ -1,8 +1,21 @@
 import requests
 import json
 import geopy.distance
+import s3
 from keys import YANKEE_API_KEY
+from botocore.exceptions import ClientError
 
+BUCKET = "tm-shuttle-positions"
+KEY = "yankee/last_shuttle_positions.csv"
+
+def load_bus_positions_from_s3():
+    try:
+        data = s3.download(BUCKET, KEY, compressed=False)
+
+        return data
+    except ClientError as ex:
+        if ex.response["Error"]["Code"] != "NoSuchKey":
+            raise
 
 
 def update_shuttles():
@@ -17,6 +30,7 @@ def update_shuttles():
 
     buses = json.loads(response.text)["data"]
 
+    last_bus_positions = load_bus_positions_from_s3()
     bus_positions = {}
 
     print(buses)
@@ -25,7 +39,22 @@ def update_shuttles():
         name = bus["name"]
         long = bus["location"]["longitude"]
         lat = bus["location"]["latitude"]
-        if name not in bus_positions:
-            bus_positions[name] = [0]
-            continue
+
+        last_long = None
+        last_lat = None
+
+        if name in last_bus_positions:
+            # do calculation of distance
+            last_lat = last_bus_positions[name]["latitude"]
+            last_long = last_bus_positions[name]["longitude"]
+
+            last_coords = (float(last_lat), float(last_long))
+            coords = (float(lat), float(long))
+
+            dist = geopy.distance.geodesic(last_coords, coords).miles
+            # persist distance to distance table
+
+        bus_positions[name] = { "latitude": lat, "longitude": long }
+
+
 
