c88e267bd3f5ae24ca6eb5ba0525f035446d6b1d
JD Rudie <rudiejd@miamioh.edu>
2024-02-04 09:47:22 -0500

    start integrating gtfs

--- ingestor/chalicelib/yankee.py
+++ ingestor/chalicelib/yankee.py
@@ -9,11 +9,23 @@ import s3
 from keys import YANKEE_API_KEY
 from botocore.exceptions import ClientError
 from datetime import datetime
+from tempfile import TemporaryDirectory
+
+from typing import List
+from sqlalchemy.orm import Session
+from mbta_gtfs_sqlite import MbtaGtfsArchive
+from mbta_gtfs_sqlite.models import (
+    RoutePattern,
+    RoutePatternTypicality,
+    Trip,
+    ShapePoint,
+)
 
 BUCKET = "tm-shuttle-positions"
 KEY = "yankee/last_shuttle_positions.csv"
-BOSTON_COORDS = (42.361145, -71.057083)
+BOSTON_COORDS = (-71.057083, 42.361145)
 OSRM_DISTANCE_API = "http://router.project-osrm.org/route/v1/driving/"
+METERS_PER_MILE = 0.000621371
 
 def load_bus_positions():
     file_name = "bus_positions.csv"
@@ -30,6 +42,54 @@ def load_bus_positions():
     # except ClientError as ex:
     #     if ex.response["Error"]["Code"] != "NoSuchKey":
     #         raise
+
+
+def get_shuttle_shapes(
+    session: Session,
+) -> List[ShapePoint]:
+    route_patterns = (
+        session.query(RoutePattern)
+        .filter(
+            RoutePattern.route_pattern_typicality == RoutePatternTypicality.DIVERSION,
+        )
+        .first()
+    )
+    print(route_patterns)
+    # if route_pattern == None:
+    #     print(f"Unable to fetch route patttern for route id {route_id}")
+    #     return []
+    #
+    # representative_trip = (
+    #     session.query(Trip)
+    #     .filter(Trip.trip_id == route_pattern.representative_trip_id)
+    #     .first()
+    # )
+    #
+    # if representative_trip == None:
+    #     print(f"Unable to fetch route patttern for route id {route_id}")
+    #     return []
+    #
+    # shape_points = (
+    #     session.query(ShapePoint)
+    #     .filter(ShapePoint.shape_id == representative_trip.shape_id)
+    #     .order_by(ShapePoint.shape_pt_sequence)
+    # ).all()
+    # return shape_points
+
+
+def get_session_for_latest_feed() -> Session:
+    archive = MbtaGtfsArchive(local_archive_path=TemporaryDirectory().name)
+    latest_feed = archive.get_latest_feed()
+    latest_feed.download_or_build()
+    return latest_feed.create_sqlite_session()
+
+
+def get_shuttle_route_shape():
+    session = get_session_for_latest_feed()
+    shape_points = get_shuttle_shapes(session)
+    # for point in shape_points:
+    #     print((point.shape_pt_lat, point.shape_pt_lon))
+
 
 def save_bus_positions(bus_positions):
     now_str = datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
@@ -48,7 +108,7 @@ Args:
     new_coords: second position
 
 Returns:
-    The distance in miles between the coordinate pairs
+    The distance in miles between the coordinate pairs, or None if the request failed
 
 Uses the API from http://project-osrm.org/docs/v5.5.1/api/#route-service
 
@@ -84,6 +144,7 @@ Example response from API:
 ```json
 """
 def get_driving_distance(old_coords: Tuple[float, float], new_coords: Tuple[float, float]) -> Optional[float]:
+    # coords must be in order (longitude, latitude)
     url = f"{OSRM_DISTANCE_API}/{old_coords[0]},{old_coords[1]};{new_coords[0]},{new_coords[1]}?overview=false"
 
     response = requests.get(url)
@@ -101,7 +162,7 @@ def get_driving_distance(old_coords: Tuple[float, float], new_coords: Tuple[floa
 
     print(response_json)
 
-    return response_json["routes"][0]["distance"]
+    return float(response_json["routes"][0]["distance"]) * METERS_PER_MILE
 
 def update_shuttles(last_bus_positions):
     url = "https://api.samsara.com/fleet/vehicles/locations"
@@ -113,6 +174,7 @@ def update_shuttles(last_bus_positions):
 
     response = requests.get(url, headers=headers)
     buses = json.loads(response.text)["data"]
+    print(buses)
     bus_positions = []
 
     for bus in buses:
@@ -120,15 +182,9 @@ def update_shuttles(last_bus_positions):
         long = bus["location"]["longitude"]
         lat = bus["location"]["latitude"]
 
-        coords = (float(lat), float(long))
+        coords = (float(long), float(lat))
 
-        # yankee gives us buses all over the place
-        # only look at buses < 5 miles from boston 
-        if geopy.distance.geodesic(BOSTON_COORDS, coords).miles > 5:
-            continue
-
-        last_long = None
-        last_lat = None
+        # skip buses that aren't in a shuttle shape
 
         dist = 0
         for pos in last_bus_positions:
@@ -137,12 +193,13 @@ def update_shuttles(last_bus_positions):
                 last_lat = pos["latitude"]
                 last_long = pos["longitude"] 
 
-                last_coords = (float(last_lat), float(last_long))
+                last_coords = (float(last_long), float(last_lat))
 
-                # accumulate distance traveled
-                dist = get_driving_distance(last_coords, coords)
-                dist += pos["distance_travelled"]
-
+                if last_coords != coords:
+                    # accumulate distance traveled
+                    dist = get_driving_distance(last_coords, coords)
+                    dist += pos["distance_travelled"]
+                print (f"Calculated distance {dist}")
         bus_positions.append({ "name": name, "latitude": lat, "longitude": long, "size": 5, "color": "red", "distance_travelled": dist})
 
     return bus_positions
