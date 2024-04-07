5e693d568c0cd387ae1d19bebece89e51da0e12f
Hamima <nasrin.hamima@gmail.com>
2024-04-07 11:37:56 -0400

    use previous stop id for departures

--- ingestor/chalicelib/lamp/ingest.py
+++ ingestor/chalicelib/lamp/ingest.py
@@ -12,7 +12,7 @@ LAMP_INDEX_URL = "https://performancedata.mbta.com/lamp/subway-on-time-performan
 RAPID_DAILY_URL_TEMPLATE = "https://performancedata.mbta.com/lamp/subway-on-time-performance-v1/{YYYY_MM_DD}-subway-on-time-performance-v1.parquet"
 S3_BUCKET = "datadashboard-backend-beta"
 # month and day are not zero-padded
-S3_KEY_TEMPLATE = "Events/rapid-data/{stop_id}/Year={YYYY}/Month={_M}/Day={_D}/events.csv"
+S3_KEY_TEMPLATE = "Events/daily-data/{stop_id}/Year={YYYY}/Month={_M}/Day={_D}/events.csv"
 
 
 # LAMP columns to fetch from parquet files
@@ -44,7 +44,56 @@ OUTPUT_COLUMNS = [
 ]
 
 
+def _process_arrivals_departure_times(pq_df: pd.DataFrame) -> pd.DataFrame:
+    """Process and collate arrivals and departures for a timetable of events.
+
+    Before: TODO add example
+    After: TODO add example
+    """
+    # NB: While generally, we can trust df dtypes fetched from parquet files as the files are compressed with columnar metadata,
+    # theres some numerical imprecisions that numpy seem to be throwing on M1 machines
+    # that are affecting how epoch timestamps are being cased to datetimes. Maybe not a problem on the AWS machines, though?
+    pq_df["dep_time"] = pd.to_datetime(pq_df["move_timestamp"], unit="s", utc=True).dt.tz_convert("US/Eastern")
+    pq_df["arr_time"] = pd.to_datetime(pq_df["stop_timestamp"], unit="s", utc=True).dt.tz_convert("US/Eastern")
+
+    # explode departure and arrival times
+    arr_df = pq_df[pq_df["arr_time"].notna()]
+    arr_df = arr_df.assign(event_type="ARR").rename(columns={"arr_time": "event_time"})
+    arr_df = arr_df[OUTPUT_COLUMNS]
+
+    dep_df = pq_df[pq_df["dep_time"].notna()]
+    dep_df = dep_df.assign(event_type="DEP").rename(columns={"dep_time": "event_time"}).drop(columns=["arr_time"])
+
+    # these departures are from the the previous stop! so set them to the previous stop id
+    # find the stop id for the departure whose sequence number precences the recorded one
+    # stop sequences don't necessarily increment by 1 or with a reliable pattern
+    dep_df = dep_df.sort_values(by=["stop_sequence"])
+    dep_df = pd.merge_asof(
+        dep_df,
+        dep_df,
+        on=["stop_sequence"],
+        by=[
+            "service_date",  # comment for faster performance
+            "route_id",
+            "trip_id",
+            "vehicle_id",
+            "vehicle_label",  # comment for faster performance
+            "direction_id",
+            "event_type",  # comment for faster performance
+        ],
+        direction="backward",
+        suffixes=("_curr", "_prev"),
+        allow_exact_matches=False,  # don't want to match on itself
+    )
+    # use CURRENT time, but PREVIOUS stop id
+    dep_df = dep_df.rename(columns={"event_time_curr": "event_time", "stop_id_prev": "stop_id"})[OUTPUT_COLUMNS]
+
+    # stitch together arrivals and departures
+    return pd.concat([arr_df, dep_df])
+
+
 def fetch_pq_file_from_remote(service_date: date) -> pd.DataFrame:
+    # TODO(check if file exists in index, throw if it doesn't)
     url = RAPID_DAILY_URL_TEMPLATE.format(YYYY_MM_DD=service_date.strftime("%Y-%m-%d"))
     result = requests.get(url)
     return pd.read_parquet(io.BytesIO(result.content), columns=INPUT_COLUMNS, engine="pyarrow")
@@ -52,26 +101,11 @@ def fetch_pq_file_from_remote(service_date: date) -> pd.DataFrame:
 
 def ingest_pq_file(pq_df: pd.DataFrame) -> pd.DataFrame:
     """Process and tranform columns for the full day's events."""
-    # NB: While generally, we can trust df dtypes fetched from parquet files as the files are compressed with columnar metadata,
-    # theres some numerical imprecisions that numpy seem to be throwing on M1 machines
-    # that are affecting how epoch timestamps are being cased to datetimes. Maybe not a problem on the AWS machines, though?
-    pq_df["dep_time"] = pd.to_datetime(pq_df["move_timestamp"], unit="s", utc=True).dt.tz_convert("US/Eastern")
-    pq_df["arr_time"] = pd.to_datetime(pq_df["stop_timestamp"], unit="s", utc=True).dt.tz_convert("US/Eastern")
     pq_df["direction_id"] = pq_df["direction_id"].astype("int16")
     pq_df["service_date"] = pq_df["service_date"].apply(format_dateint)
 
-    # explode and stack departure and arrival times
-    arr_df = pq_df[pq_df["arr_time"].notna()]
-    arr_df = arr_df.assign(event_type="ARR").rename(columns={"arr_time": "event_time"}).drop(columns=["dep_time"])
-    dep_df = pq_df[pq_df["dep_time"].notna()]
-    dep_df = dep_df.assign(event_type="DEP").rename(columns={"dep_time": "event_time"}).drop(columns=["arr_time"])
-
-    # stitch together arrivals and departures
-    # TODO: sort by event_time?
-    processed_daily_events = pd.concat([arr_df, dep_df])
-
-    # drop intermediate inference columns
-    return processed_daily_events[OUTPUT_COLUMNS]
+    processed_daily_events = _process_arrivals_departure_times(pq_df)
+    return processed_daily_events.sort_values(by=["event_time"])
 
 
 def _local_save(s3_key, stop_events):
@@ -91,7 +125,7 @@ def upload_to_s3(stop_id_and_events: Tuple[str, pd.DataFrame], service_date: dat
     s3_key = S3_KEY_TEMPLATE.format(stop_id=stop_id, YYYY=service_date.year, _M=service_date.month, _D=service_date.day)
     # s3.upload_df_as_csv(S3_BUCKET, s3_key, stop_events)
     _local_save(s3_key, stop_events)
-    return [True]
+    return [stop_id]
 
 
 _parallel_upload = make_parallel(upload_to_s3)
@@ -104,7 +138,7 @@ def ingest_lamp_data():
 
     # split daily events by stop_id and parallel upload to s3
     stop_event_groups = processed_daily_events.groupby("stop_id")
-    a = _parallel_upload(stop_event_groups, service_date)
+    _parallel_upload(stop_event_groups, service_date)
 
 
 if __name__ == "__main__":
