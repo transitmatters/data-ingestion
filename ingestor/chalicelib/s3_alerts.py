42221b9db4dbc5337abcdc07457c77f84f2e97ac
Devin Matte <devinmatte@gmail.com>
2024-04-08 11:08:22 -0400

    Adding TODO comments

--- ingestor/chalicelib/s3_alerts.py
+++ ingestor/chalicelib/s3_alerts.py
@@ -10,6 +10,9 @@ def key(day):
 
 
 def store_alerts(day):
+    # TODO: Replace with v3 API calls
+    # V3 call only store same day and upcoming alerts, so we'll need to change how this runs
+    # We may need to call it hourly during the service day to get all alerts
     api_data = MbtaPerformanceAPI.get_api_data("pastalerts", {}, day)
     alerts = json.dumps(api_data).encode("utf8")
     s3.upload(BUCKET, key(day), alerts, True)
