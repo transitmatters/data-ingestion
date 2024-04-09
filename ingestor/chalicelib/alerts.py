d57526508f6751789b4c21225799cb23cb3b8e4e
Devin Matte <devinmatte@gmail.com>
2024-04-08 20:21:43 -0400

    Start storing alerts every 15 minutes during the service day

---
+++ ingestor/chalicelib/alerts.py
@@ -0,0 +1,44 @@
+from datetime import date
+import json
+from typing_extensions import deprecated
+
+import requests
+
+from chalicelib import MbtaPerformanceAPI, s3
+from chalicelib.date_utils import get_current_service_date
+from botocore.exceptions import ClientError
+
+BUCKET = "tm-mbta-performance"
+
+
+def key(day, v3: bool = False):
+    if v3:
+        return f"Alerts/v3/{str(day)}.json.gz"
+    return f"Alerts/{str(day)}.json.gz"
+
+
+@deprecated("This uses the performance API, we should move to v3 alerts")
+def store_alerts(day: date):
+    api_data = MbtaPerformanceAPI.get_api_data("pastalerts", {}, day)
+    alerts = json.dumps(api_data).encode("utf8")
+    s3.upload(BUCKET, key(day), alerts, True)
+
+
+def save_v3_alerts():
+    r_s = requests.get("https://api-v3.mbta.com/alerts")
+    alerts = r_s.json()
+
+    service_date = get_current_service_date()
+    try:
+        all_alerts = s3.download(BUCKET, key(service_date), encoding="utf8", compressed=True)
+    except ClientError as ex:
+        if ex.response["Error"]["Code"] != "NoSuchKey":
+            raise
+        all_alerts = dict()
+
+    all_alerts = dict()
+    for alert in alerts["data"]:
+        all_alerts[alert["id"]] = alert
+
+    alert_json = json.dumps(all_alerts).encode("utf8")
+    s3.upload(BUCKET, key(service_date), alert_json, compress=True)
