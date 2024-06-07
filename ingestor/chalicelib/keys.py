1d3fd08fab50adb91cc862eaeda8f98966634496
Devin Matte <devinmatte@gmail.com>
2024-06-07 16:39:09 -0400

    Remove performance api (#102)

--- ingestor/chalicelib/keys.py
+++ ingestor/chalicelib/keys.py
@@ -1,7 +1,6 @@
 #  This file can't be named secrets.py: https://github.com/numpy/numpy/issues/14860
 import os
 
-MBTA_V2_API_KEY = os.environ.get("MBTA_V2_API_KEY", "")
 MBTA_V3_API_KEY = os.environ.get("MBTA_V3_API_KEY", "")
 
 YANKEE_API_KEY = os.environ.get("YANKEE_API_KEY", "")
