96653181a308d057c6f033a36ae19471b52e69db
Devin Matte <devinmatte@gmail.com>
2025-09-18 11:03:14 -0400

    Fixing ridership regular ingest (#141)

--- ingestor/chalicelib/ridership/box.py
+++ ingestor/chalicelib/ridership/box.py
@@ -2,19 +2,9 @@ import boto3
 import json
 from datetime import date
 from re import Pattern
-from tempfile import NamedTemporaryFile
-from typing import List, Tuple
 from boxsdk import OAuth2, Client
 from boxsdk.object.file import File
-import requests
-
-from .config import (
-    CR_RIDERSHIP_ARCGIS_URL,
-    FERRY_RIDERSHIP_ARCGIS_URL,
-    RIDERSHIP_BOX_URL,
-    RIDERSHIP_BUS_XLSX_REGEX,
-    RIDERSHIP_SUBWAY_CSV_REGEX,
-)
+from typing import List
 
 
 def get_secret_access_token():
@@ -43,41 +33,3 @@ def get_file_matching_date_pattern(files: List[File], pattern: Pattern):
             day = match[3]
             file_date = date(year=int(year), month=int(month), day=int(day))
             return file, file_date
-
-
-def download_latest_ridership_files(
-    require_matching_dates=False,
-) -> Tuple[str, str, str, str]:
-    client = get_box_client()
-    folder = client.get_shared_item(RIDERSHIP_BOX_URL)
-    files = list(folder.get_items())
-    maybe_bus_file_and_date = get_file_matching_date_pattern(
-        files,
-        RIDERSHIP_BUS_XLSX_REGEX,
-    )
-    maybe_subway_file_and_date = get_file_matching_date_pattern(
-        files,
-        RIDERSHIP_SUBWAY_CSV_REGEX,
-    )
-    if maybe_bus_file_and_date and maybe_subway_file_and_date:
-        subway_file, subway_date = maybe_subway_file_and_date
-        bus_file, bus_date = maybe_bus_file_and_date
-        assert (
-            not require_matching_dates or bus_date == subway_date
-        ), f"Mismatched file dates: {bus_date} and {subway_date}"
-        subway_tmp_path = NamedTemporaryFile().name
-        bus_tmp_path = NamedTemporaryFile().name
-        cr_tmp_path = NamedTemporaryFile().name
-        ferry_tmp_path = NamedTemporaryFile().name
-        with open(subway_tmp_path, "wb") as file:
-            subway_file.download_to(file)
-        with open(bus_tmp_path, "wb") as file:
-            bus_file.download_to(file)
-        with open(cr_tmp_path, "wb") as file:
-            req = requests.get(CR_RIDERSHIP_ARCGIS_URL)
-            file.write(req.content)
-        with open(ferry_tmp_path, "wb") as file:
-            req = requests.get(FERRY_RIDERSHIP_ARCGIS_URL)
-            file.write(req.content)
-        return subway_tmp_path, bus_tmp_path, cr_tmp_path, ferry_tmp_path
-    raise Exception("Could not find ridership data files")
