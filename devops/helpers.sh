88a16bf4d44dbfd965b80ec5814ab4853de4f301
Devin Matte <devinmatte@gmail.com>
2025-11-25 11:44:40 -0500

    Output or true on zip cleanup

--- devops/helpers.sh
+++ devops/helpers.sh
@@ -2,79 +2,79 @@
 
 # Shrink the deployment package for the lambda layer https://stackoverflow.com/a/69355796
 function shrink {
-    zip -d -qq cfn/layer-deployment.zip '*/__pycache__/*'
-    zip -d -qq cfn/layer-deployment.zip '**/*.pyc'
-    zip -d -qq cfn/layer-deployment.zip '**/LICENSE*'
-    zip -d -qq cfn/layer-deployment.zip '**/AUTHOR*'
-    zip -d -qq cfn/layer-deployment.zip '**/NOTICE*'
-    zip -d -qq cfn/layer-deployment.zip '**/*.md'
-    zip -d -qq cfn/layer-deployment.zip '**/*.c'
-    zip -d -qq cfn/layer-deployment.zip '**/*.cpp'
-    zip -d -qq cfn/layer-deployment.zip '**/*.h'
-    zip -d -qq cfn/layer-deployment.zip '**/*.pyx'
-    zip -d -qq cfn/layer-deployment.zip '**/*.pxd'
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/numpy*/tests/**/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/pandas*/tests/**/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/pandas/tests/**/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/pandas/_libs/tslibs/src/datetime/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/pandas/io/formats/templates/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/examples/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/sqlalchemy*/testing/**/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/sqlalchemy/dialects/oracle/**/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/sqlalchemy/dialects/mssql/**/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/sqlalchemy/dialects/mysql/**/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/sqlalchemy/dialects/postgresql/**/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/*/zoneinfo/Africa/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/*/zoneinfo/Asia/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/*/zoneinfo/Atlantic/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/*/zoneinfo/Arctic/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/*/zoneinfo/Antarctica/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/*/zoneinfo/Australia/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/*/zoneinfo/Brazil/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/*/zoneinfo/Chile/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/*/zoneinfo/Europe/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/*/zoneinfo/Indian/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/*/zoneinfo/Pacific/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/*/zoneinfo/Mexico/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/*/zoneinfo/Singapore
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/*/zoneinfo/Turkey
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/*/zoneinfo/Poland
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/*/zoneinfo/Egypt
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/*/zoneinfo/Hongkong
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/*/zoneinfo/I*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/*/zoneinfo/J*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/a*/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/b*/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/co*/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/ch*/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/cu*/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/da*/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/di*/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/do*/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/de*/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/e*/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/f*/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/g*/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/h*/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/i*/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/j*/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/k*/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/l*/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/m*/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/n*/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/o*/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/p*/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/q*/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/r*/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/sa*/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/ss*/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/t*/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/u*/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/v*/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/w*/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/x*/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/y*/*
-    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/z*/*
+    zip -d -qq cfn/layer-deployment.zip '*/__pycache__/*' || true
+    zip -d -qq cfn/layer-deployment.zip '**/*.pyc' || true
+    zip -d -qq cfn/layer-deployment.zip '**/LICENSE*' || true
+    zip -d -qq cfn/layer-deployment.zip '**/AUTHOR*' || true
+    zip -d -qq cfn/layer-deployment.zip '**/NOTICE*' || true
+    zip -d -qq cfn/layer-deployment.zip '**/*.md' || true
+    zip -d -qq cfn/layer-deployment.zip '**/*.c' || true
+    zip -d -qq cfn/layer-deployment.zip '**/*.cpp' || true
+    zip -d -qq cfn/layer-deployment.zip '**/*.h' || true
+    zip -d -qq cfn/layer-deployment.zip '**/*.pyx' || true
+    zip -d -qq cfn/layer-deployment.zip '**/*.pxd' || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/numpy*/tests/**/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/pandas*/tests/**/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/pandas/tests/**/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/pandas/_libs/tslibs/src/datetime/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/pandas/io/formats/templates/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/examples/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/sqlalchemy*/testing/**/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/sqlalchemy/dialects/oracle/**/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/sqlalchemy/dialects/mssql/**/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/sqlalchemy/dialects/mysql/**/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/sqlalchemy/dialects/postgresql/**/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/*/zoneinfo/Africa/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/*/zoneinfo/Asia/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/*/zoneinfo/Atlantic/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/*/zoneinfo/Arctic/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/*/zoneinfo/Antarctica/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/*/zoneinfo/Australia/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/*/zoneinfo/Brazil/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/*/zoneinfo/Chile/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/*/zoneinfo/Europe/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/*/zoneinfo/Indian/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/*/zoneinfo/Pacific/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/*/zoneinfo/Mexico/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/*/zoneinfo/Singapore || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/*/zoneinfo/Turkey || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/*/zoneinfo/Poland || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/*/zoneinfo/Egypt || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/*/zoneinfo/Hongkong || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/*/zoneinfo/I* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/*/zoneinfo/J* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/a*/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/b*/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/co*/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/ch*/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/cu*/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/da*/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/di*/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/do*/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/de*/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/e*/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/f*/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/g*/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/h*/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/i*/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/j*/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/k*/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/l*/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/m*/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/n*/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/o*/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/p*/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/q*/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/r*/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/sa*/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/ss*/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/t*/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/u*/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/v*/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/w*/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/x*/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/y*/* || true
+    zip -d -qq cfn/layer-deployment.zip python/lib/**/site-packages/boto*/data/z*/* || true
 }
 
 # Check if the layer deployment package is under the maximum size
